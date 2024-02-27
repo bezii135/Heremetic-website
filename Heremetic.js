6d6e23752966431ae055af263aebafa3e51aeb87
beza <bzendale6@gmail.com>
2024-02-27 13:40:49 +0100

    try

--- Heremetic.js
+++ Heremetic.js
@@ -30,3 +30,13 @@ window.onscroll =() =>{
     searchForm.classList.remove('active');
     contactInfo.classList.remove('active');
 }
+
+var swiper = new Swiper(".home-slider", {
+    loop:true,
+    grabCursor:true,
+    
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
